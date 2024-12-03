05a377aeab9029ef7f6d4e2449f35b9a10eb4eae
snjrbkv <sanjarbekovodilbek@gmail.com>
2024-12-03 10:02:02 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function formatPhone(input) {
-  let value = input.value.replace(/\D/g, ""); // Убираем всё, кроме цифр
+  let value = input.value.replace(/\D/g, "");
   let formatted = "+7 (";
   if (value.length > 1) formatted += value.slice(1, 4);
   if (value.length > 4) formatted += ") " + value.slice(4, 7);
@@ -7,32 +7,32 @@ function formatPhone(input) {
   if (value.length > 9) formatted += "-" + value.slice(9, 11);
   input.value = formatted;
 }
-// Инициализация EmailJS с вашим Public Key
 (function () {
-  emailjs.init("91aW48iwi13jG7p45"); // Замените на ваш Public Key
+  emailjs.init("0-4Y8Yo73zzKWb15U");
 })();
 
-// Функция для отправки данных формы
 async function sendForm(event) {
-  event.preventDefault(); // Остановить стандартное поведение формы
-
   const phoneInput = document.querySelector(".hero-form__num-input").value;
-
-  if (!phoneInput.trim()) {
-    alert("Пожалуйста, введите номер телефона!");
+  event.preventDefault();
+  if (!phoneInput.trim() || phoneInput.length < 18) {
+    alert("Введите корректный номер телефона!");
     return;
   }
 
   try {
-    // Отправка данных через EmailJS
-    const response = await emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
-      user_phone: phoneInput,
+    const response = await emailjs.send("service_87ptpxi", "template_2m0exo2", {
+      to_name: "Команда",
+      from_name: "Клиент",
+      message: `Номер телефона: ${phoneInput}`,
     });
-    alert("Ваше сообщение успешно отправлено!");
+    alert("Сообщение успешно отправлено!");
+    phoneInput.value = "";
   } catch (error) {
     alert("Ошибка при отправке сообщения: " + error.text);
   }
 }
 
-// Подключение функции отправки к форме
 document.getElementById("contactForm").addEventListener("submit", sendForm);
+function openModal() {
+  document.body.classList.toggle("gallery-active");
+}
